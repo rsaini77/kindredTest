1807ef218bd0e131d5a7fc3af000db7940135444
Rajeev Saini <rsaini77@gmail.com>
2021-08-15 10:51:35 +1000

    Putting the implementation in the service so that the One Spare Test can pass.

--- BowlingCalcAPI/Services/GameService.cs
+++ BowlingCalcAPI/Services/GameService.cs
@@ -7,14 +7,44 @@ namespace BowlingCalcAPI.Services
 {
     public class GameService
     {
-        public int score;
+        private int currentRoll;
+        private int[] rolls = new int[21];
         public void Roll(int pins)
         {
-            score += pins;
+            rolls[currentRoll++] = pins;
         }
         public int Score()
         {
+            int score = 0;
+            int roll = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(roll))
+                {
+                    score += 10
+                        + SpareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(roll);
+                    roll += 2;
+                }
+            }
             return score;
+        }
+        private bool IsSpare(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+
+        private int SumOfBallsInFrame(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1];
+        }
+        private int SpareBonus(int roll)
+        {
+            return rolls[roll + 2];
         }
     }
 
