415704ba6c8b39c70bcd75b4f54a1cc1fb6ef97e
Rajeev Saini <rsaini77@gmail.com>
2021-08-15 10:27:12 +1000

    Adding the GameService

--- BowlingCalcAPITest/Services/GameServiceTest.cs
+++ BowlingCalcAPITest/Services/GameServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BowlingCalcAPI.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,6 +8,12 @@ namespace BowlingCalcAPITest.Services
 {
     public class GameServiceTest
     {
+        GameService game;
+
+        public GameServiceTest()
+        {
+            game = new GameService();
+        }
         [Fact]
         public void GutterGameTest()
         {
