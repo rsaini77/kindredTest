493a3c8d71235fcbbc5f48f7b79934f5ec06f66f
Rajeev Saini <rsaini77@gmail.com>
2021-08-15 10:22:09 +1000

    Adding the gutter game test

---
+++ BowlingCalcAPITest/Services/GameServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BowlingCalcAPITest.Services
+{
+    public class GameServiceTest
+    {
+        [Fact]
+        public void GutterGameTest()
+        {
+
+        }
+    }
+}
